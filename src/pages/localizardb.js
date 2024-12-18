2ba657bd0aa35c42be79596f6a2b953d2f56c19e
DevTec2020 <leonardo.silva.inf@gmail.com>
2024-12-18 20:42:38 -0300

    Novo layout para a tela de localizar

--- src/pages/localizardb.js
+++ src/pages/localizardb.js
@@ -3,64 +3,68 @@ export function localizardbClick() {
 
     DivMain.innerHTML = `
         <!-- cabeçalho da tela--> 
-        <div>
-          <h3 class="text-lg font-semibold text-slate-800">Lista de bancos</h3>
-          <p class="text-slate-500">Relação de bancos para consulta</p>
-        </div>
+      <div class="mb-10">
+        <h3 class="text-lg font-semibold text-slate-800">Lista de bancos</h3>
+        <p class="text-slate-500">Relação de bancos para consulta</p>
+      </div>
 
-        <!-- Form de busca-->
-         <div class="flex mt-5 justify-end">
-            <form class="flex w-80 shadow">
-              <img class="mx-3" src="./src/assets/Icone-search.svg" alt="" srcset="" />
-              <input class="h-12 w-full text-center" type="text" placeholder="Digite o número do chamado "/>
-            </form>
-        </div>
+      <!-- Form de busca-->
+       <div class="flex mt-5">
+          <form class="flex w-full border rounded-md shadow">
+            <img class="mx-3" src="./src/assets/Icone-search.svg" alt="" srcset="" />
+            <input class="h-10 w-full focus:outline-none" type="text" placeholder="Procurar por chamado"/>
+          </form>
+      </div>
 
-        <!-- tabela de dados-->
-        <div class="mt-5 shadow-md rounded-lg">
-          <table class="w-full text-center">
-            <thead class="bg-gray-300">
-              <tr>
-                <th>#</th>
-                <th>Data</th>
-                <th>Chamado</th>
-                <th>Solicitante</th>
-                <th>Caminho do Banco</th>
-                <th>Pode Excluir?</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th>1</th>
-                <td>teste</td>
-                <td>teste</td>
-                <td>teste</td>
-                <td>teste</td>
-                <td>teste</td>
-                <td>teste</td>
-              </tr>
-              <tr>
-                <th>2</th>
-                <td>teste</td>
-                <td>teste</td>
-                <td>teste</td>
-                <td>teste</td>
-                <td>teste</td>
-                <td>teste</td>
-              </tr>
-              <tr>
-                <th>3</th>
-                <td>teste</td>
-                <td>teste</td>
-                <td>teste</td>
-                <td>teste</td>
-                <td>teste</td>
-                <td>teste</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      <!-- tabela de dados-->
+      <div class="mt-5 shadow-md rounded-lg">
+        <table class="w-full text-center">
+          <thead class="bg-gray-100 font-semibold">
+            <tr>
+              <td><input class=" rounded" type="checkbox"></td>
+              <td>Data</td>
+              <td>Chamado</td>
+              <td>Servidor</td>
+              <td>Banco</td>
+              <td>Solicitante</td>
+              <td>Pode Excluir?</td>
+              <td>Status</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th><input type="checkbox"></th>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+            </tr>
+            <tr>
+              <th><input type="checkbox"></th>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+            </tr>
+            <tr>
+              <th><input type="checkbox"></th>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     ` 
 }
 
