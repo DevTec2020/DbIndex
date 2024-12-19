a8c070c3365c5f081e6bb1bd4f7b8960c515912b
DevTec2020 <leonardo.silva.inf@gmail.com>
2024-12-18 23:21:15 -0300

    Criado Json com dados e mostrando os dados a tabela - page localizar

--- src/pages/localizardb.js
+++ src/pages/localizardb.js
@@ -3,9 +3,18 @@ export function localizardbClick() {
 
     DivMain.innerHTML = `
         <!-- cabeçalho da tela--> 
-      <div class="mb-10">
-        <h3 class="text-lg font-semibold text-slate-800">Lista de bancos</h3>
-        <p class="text-slate-500">Relação de bancos para consulta</p>
+      <div class="mb-10 flex justify-between items-center">
+        <div>
+          <h3 class="text-lg font-semibold text-slate-800">Lista de bancos</h3>
+          <p class="text-slate-500">Relação de bancos para consulta</p>
+        </div>
+        <div class="h-9 flex items-center gap-3 px-3 bg-red-600 text-white rounded-lg cursor-pointer">
+          <span class="font-bold">Ações</span>
+          <svg class="w-3 h-3 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 10">
+            <path d="M15.434 1.235A2 2 0 0 0 13.586 0H2.414A2 2 0 0 0 1 3.414L6.586 9a2 2 0 0 0 2.828 0L15 3.414a2 2 0 0 0 .434-2.179Z"/>
+        </svg>
+        </div>
+        
       </div>
 
       <!-- Form de busca-->
@@ -31,41 +40,37 @@ export function localizardbClick() {
               <td>Status</td>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <th><input type="checkbox"></th>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-            </tr>
-            <tr>
-              <th><input type="checkbox"></th>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-            </tr>
-            <tr>
-              <th><input type="checkbox"></th>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-            </tr>
+          <tbody id="bodyTable">
+            
           </tbody>
         </table>
       </div>
     ` 
+    let bodyTable = document.querySelector("#bodyTable")
+
+    // Acessando o Json com o fetch
+    fetch('./src/data/db.json')
+      .then((response) => response.json())
+      .then((dados) => {
+        // Percorre o array do json e cria os li
+        dados.restores.forEach((restore) => {
+          bodyTable.innerHTML += `
+          <tr class="hover:bg-gray-200 hover:cursor-default">
+            <th><input type="checkbox"></th>
+            <td>${restore.data}</td>
+            <td>${restore.chamado}</td>
+            <td>${restore.servidor}</td>
+            <td>${restore.banco}</td>              
+            <td>${restore.solicitante}</td>
+            <td>${restore.podeExcluir}</td>
+            <td>${restore.status}</td>
+          </tr>
+          `
+        })
+      })
+      .catch((error) => {
+        console.log("Erro ao carregar os dados:", error)
+      })
 }
 
 
